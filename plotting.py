72f4cbf1d537ed437a6fc603970e58d07bb47600
frpezzolla <frpezzoll@gmail.com>
2024-08-26 18:47:52 -0400

    added quasi-empty main, plotting, processing and requirements files to define structure

---
+++ plotting.py
@@ -0,0 +1,10 @@
+import matplotlib
+import matplotlib.pyplot as plt
+
+# Set LaTeX formatting for plotting
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+matplotlib.rcParams.update({'font.size': 14})
+
+if __name__ == "__main__":
+    pass
---
+++ processing.py
@@ -0,0 +1,9 @@
+import csv
+import numpy as np
+import pandas as pd
+import scipy as sc
+
+RANDOM_SEED = 0
+
+if __name__ == "__main__":
+    pass
