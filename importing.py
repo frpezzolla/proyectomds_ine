1949c1552f92c0c96228a8fa691388f46c549476
frpezzolla <frpezzoll@gmail.com>
2024-08-28 13:53:55 -0400

    importing now only returns concatenated dataframe through import_data()

--- importing.py
+++ importing.py
@@ -11,36 +11,58 @@ coloredlogs.install(level='DEBUG')
 
 #%%
 
-data = {}
-
-# for year in range(10, 24):
-#     data[f'20{year}'] = pd.read_csv(
+def import_data():
+    data = {}
+    
+    # for year in range(10, 24):
+    #     data[f'20{year}'] = pd.read_csv(
+    #         os.path.join(
+    #             'data',
+    #             f'ano-20{year}.csv'), 
+    #         encoding='latin-1',
+    #         sep=';'
+    #     )
+    
+    logger.info('importing year 2010')
+    data = pd.read_csv(
+        os.path.join(
+            os.getcwd(),
+            'data',
+            f'ano-20{10}.csv'),
+        encoding='latin-1',
+        sep=';'
+        )
+    
+    for year in range(11, 24):
+        logger.info(f"importing year 20{year}")
+        data = pd.concat([data, 
+                    pd.read_csv(                    
+                        os.path.join(
+                            os.getcwd(),
+                            'data',
+                            f'ano-20{year}.csv'), 
+                        encoding='latin-1',
+                        sep=';'
+        )])
+    
+    # logger.info('saving to todos-anos.csv')    
+    # data.to_parquet('todos-anos.parquet', compression='snappy')
+    
+    return data
+    
+# def load_data():
+    
+#     logger.info('loading todos-anos.csv')
+    
+#     data = pd.read_parquet(
 #         os.path.join(
+#             os.getcwd(),
 #             'data',
-#             f'ano-20{year}.csv'), 
+#             'todos-anos.csv'),
 #         encoding='latin-1',
-#         sep=';'
-#     )
+#         sep=','
+#         )
+    
+#     return data
 
-logger.info('importing year 2010')
-data = pd.read_csv(
-    os.path.join(
-        os.getcwd(),
-        'data',
-        f'ano-20{10}.csv'),
-    encoding='latin-1',
-    sep=';'
-    )
-
-for year in range(11, 24):
-    logger.info(f"importing year 20{year}")
-    data = pd.concat([data, 
-                pd.read_csv(                    
-                    os.path.join(
-                        os.getcwd(),
-                        'data',
-                        f'ano-20{year}.csv'), 
-                    encoding='latin-1',
-                    sep=';'
-    )])
-
+#%%
