1d8089e66308a2fe4a424791429cf107a6f22ff8
frpezzolla <frpezzoll@gmail.com>
2024-08-28 11:40:55 -0400

    importing now concatenates all years into one dataframe | requirements.txt now includes coloredlogs

--- importing.py
+++ importing.py
@@ -2,13 +2,45 @@ import numpy as np
 import os
 import pandas as pd
 
+#%%
+import logging
+import coloredlogs
+
+logger = logging.getLogger(__name__)
+coloredlogs.install(level='DEBUG')
+
+#%%
+
 data = {}
 
-for year in range(10, 24):
-    data[f'20{year}'] = pd.read_csv(
-        os.path.join(
-            'data',
-            f'ano-20{year}.csv'), 
-        encoding='latin-1',
-        sep=';'
+# for year in range(10, 24):
+#     data[f'20{year}'] = pd.read_csv(
+#         os.path.join(
+#             'data',
+#             f'ano-20{year}.csv'), 
+#         encoding='latin-1',
+#         sep=';'
+#     )
+
+logger.info('Importing year 2010')
+data = pd.read_csv(
+    os.path.join(
+        os.getcwd(),
+        'data',
+        f'ano-20{10}.csv'),
+    encoding='latin-1',
+    sep=';'
     )
+
+for year in range(11, 24):
+    logger.info(f"importing year 20{year}")
+    data = pd.concat([data, 
+                pd.read_csv(                    
+                    os.path.join(
+                        os.getcwd(),
+                        'data',
+                        f'ano-20{year}.csv'), 
+                    encoding='latin-1',
+                    sep=';'
+    )])
+
