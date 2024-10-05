c92d93cc7a70c352d2dfa82eb64555c30000df27
Rodrigo <rodmolina@ug.uchile.cl>
2024-10-05 01:09:33 -0300

    Actualización de comentarios que describen el código

--- EDA/EDA_ENE/ano-2024.py
+++ EDA/EDA_ENE/ano-2024.py
@@ -1,18 +1,20 @@
 import pandas as pd
 
-# Lista de archivos CSV desde 2010 hasta 2023
-csv_files = [f'C:/Users/rodri/OneDrive/Escritorio/MDS/Proyecto de Ciencia de Datos/proyectomds_ine/data/ano-{year}.csv' for year in range(2010, 2024)]
-# Añadir los archivos de 2024
+# Unimos todas las ENE anualizadas disponibles en la página.
+
+csv_files = [f'ano-{year}.csv' for year in range(2010, 2024)]
+# Añadir los archivos ENE enero-febrero-marzo y abril-mayo-junio de 2024
+
 csv_files.extend(['eneefm.csv', 'eneamj.csv'])
 
-# Columnas deseadas
+# Usamos las variables: AÑO, MES, SEXO, TRAMO_EDAD, REGION, CINE, PROVEEDOR, ACTIV, B7A_1, EST_CONYUGAL, B1, TIPO, CATEGORIA_OCUPACION
+
 columns = ['ano_encuesta', 'mes_encuesta', 'sexo', 'tramo_edad', 'region', 'cine', 'proveedor',
            'activ', 'b7a_1', 'est_conyugal', 'b1', 'tipo', 'categoria_ocupacion']
 
-# Lista para almacenar los DataFrames
 data_frames = []
 
-# Leer y procesar cada archivo CSV
+# Leemos cada csv en la lista, y porsiacaso utilizamos encodings diferentes, evaluar si son necesarios, el sep=';' es propio de las ENE trimestrales
 for file in csv_files:
     try:
         df = pd.read_csv(file, usecols=columns, encoding='latin1', sep=';')
@@ -22,19 +24,17 @@ for file in csv_files:
         print(f"Error processing {file}: {e}")
         continue
     
-    # Convertir ano_encuesta y mes_encuesta a formato datetime
+    # Convertir a datetime, escencial para las visualizaciones
     df['fecha_encuesta'] = pd.to_datetime(df['ano_encuesta'].astype(str) + '-' + df['mes_encuesta'].astype(str), format='%Y-%m')
     
-    # Convertir las variables categóricas a tipo 'category'
+    # Convertir a categorías, aunque sí dudo un poco de si es formato category o string.
     categorical_columns = ['sexo', 'tramo_edad', 'region', 'cine', 'proveedor', 'activ', 'b7a_1', 'est_conyugal', 'b1', 'tipo', 'categoria_ocupacion']
     for col in categorical_columns:
         df[col] = df[col].astype('category')
     
     data_frames.append(df)
 
-# Concatenar todos los DataFrames
 combined_df = pd.concat(data_frames, ignore_index=True)
 
-# Guardar el DataFrame combinado en un nuevo archivo CSV
 combined_df.to_csv('combined_data_2024.csv', index=False)
 
